d3a3380c9ae10afca8d547ec0b53880c82bd8864
Pasha <pashadesnov@gmail.com>
2021-07-04 21:35:37 +0300

    first commit

---
+++ src/reducer/visibilityFilter.js
@@ -0,0 +1,21 @@
+export const setVisibilityFilter = (filter) => ({
+  type: "SET_VISIBILITY_FILTER",
+  filter,
+});
+
+export const VisibilityFilters = {
+  SHOW_ALL: "SHOW_ALL",
+  SHOW_COMPLETED: "SHOW_COMPLETED",
+  SHOW_ACTIVE: "SHOW_ACTIVE",
+};
+
+const visibilityFilter = (state = VisibilityFilters.SHOW_ALL, action) => {
+  switch (action.type) {
+    case "SET_VISIBILITY_FILTER":
+      return action.filter;
+    default:
+      return state;
+  }
+};
+
+export default visibilityFilter;
